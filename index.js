04d11f183215078cc324c4938ecf98b5537ad503
marknoodles <61075458+marknoodles@users.noreply.github.com>
2022-06-21 16:37:27 +0300

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+const TelegramApi = require("node-telegram-bot-api");
+
+const token = "5588604046:AAHGRPTwY95CSJYyDKTd4d4V1XsCQaAdm3E";
+
+const bot = new TelegramApi(token, { polling: true });
+
+const start = () => {
+
+  //команды на бота, которые появятся в менюшке бота
+  bot.setMyCommands([
+    { command: "/start", description: "Приветствие" },
+    { command: "/info", description: "Приветствие" },
+  ]);
+
+  bot.on("message", async (msg) => {
+
+    const text = msg.text;
+    const chatId = msg.chat.id;
+
+    if (text === "/start") {
+        // использовать await на другие условия
+      return bot.sendMessage(chatId, "Welcome to the candyshop");
+    }
+
+    if (text === "/info") {
+      return bot.sendMessage(chatId, `ur name is ${msg.from.first_name} ${msg.from.last_name}`);
+    }
+    return bot.sendMessage(chatId, `не понел`);
+  });
+};
+start();
